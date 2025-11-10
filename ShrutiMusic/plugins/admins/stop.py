5d28e66f3a8b52015a68a6ce196132d2d6bb2553
deepbotoffical <missmelih02@gmail.com>
2025-11-10 21:50:44 +0300

    Update stop.py

--- ShrutiMusic/plugins/admins/stop.py
+++ ShrutiMusic/plugins/admins/stop.py
@@ -32,7 +32,7 @@ from config import BANNED_USERS
 
 
 @app.on_message(
-    filters.command(["end", "stop", "cend", "cstop"]) & filters.group & ~BANNED_USERS
+    filters.command(["son", "stop", "kson", "ksson"]) & filters.group & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def stop_music(cli, message: Message, _, chat_id):
